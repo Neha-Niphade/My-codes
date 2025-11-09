30ef785562874a253833d0cc5f27468a67a96137
Neha Niphade <niphadeneha6@gmail.com>
2025-11-10 04:15:37 +0530

    Refactor library record system functions

--- LibM.py
+++ LibM.py
@@ -1,102 +1,115 @@
-# Global variables
-records = []
-n = 0
-
-def input_records():
-    global records, n
-    n = int(input("Enter total members: "))
-    records = [0] * n      # fixed size list
-    
-    for i in range(n):
-        x = int(input(f"Books borrowed by member {i+1}: "))
-        records[i] = x
-    
-    print("Records added.")
+def avg_(members):
+    if not members:
+        print("No members")
+        return
+    avgg = my_sum(members) / my_len(members)
+    print(avgg)
 
 
-def compute_avg():
-    if n == 0:
-        print("No data")
+def my_sum(members):
+    s = 0
+    for k in members:       # k = key
+        s = s + members[k]  # add value
+    return s
+
+
+def my_len(members):
+    c = 0
+    for _ in members:
+        c = c + 1
+    return c
+
+
+def High_Low(books):
+    if not books:
+        print("No books added yet!")
         return
 
-    total = 0
-    for i in range(n):
-        total += records[i]
+    most = None
+    mostName = None
+    least = None
+    leastName = None
 
-    avg = total / n
-    print("Average =", avg)
+    for b in books:
+        v = books[b]
+
+        if most is None or v > most:
+            most = v
+            mostName = b
+
+        if least is None or v < least:
+            least = v
+            leastName = b
+
+    print("Most borrowed book:", mostName, "(", most, "times )")
+    print("Least borrowed book:", leastName, "(", least, "times )")
 
 
-def highest_lowest():
-    if n == 0:
-        print("No data")
+def Zero_cnt(members):
+    zero = 0
+    for m in members:
+        if members[m] == 0:
+            zero = zero + 1
+    print("#members with zero boc:", zero)
+
+
+def Most_freq(books):
+    if not books:
+        print("No books")
         return
 
-    highest = records[0]
-    lowest = records[0]
+    # 1. find max value
+    max_borrow = None
+    for b in books:
+        if max_borrow is None or books[b] > max_borrow:
+            max_borrow = books[b]
 
-    for i in range(n):
-        if records[i] > highest:
-            highest = records[i]
-        if records[i] < lowest:
-            lowest = records[i]
+    # 2. collect all books having that max value
+    result = []
+    for b in books:
+        if books[b] == max_borrow:
+            result = result + [b]      # no list comprehension
 
-    print("Highest =", highest)
-    print("Lowest =", lowest)
-
-
-def zero_borrowers():
-    count0 = 0
-    for i in range(n):
-        if records[i] == 0:
-            count0 += 1
-    print("Members with 0 borrowings =", count0)
+    print("Most frequently borrowed book(s):", result)
 
 
-def find_mode():
-    if n == 0:
-        print("No data")
-        return
+def Menu():
+    books = {}
+    members = {}
 
-    freq = {}
-
-    # build frequency table
-    for i in range(n):
-        val = records[i]
-        if val not in freq:
-            freq[val] = 1
-        else:
-            freq[val] += 1
-
-    # find mode manually
-    mode = None
-    maxf = 0
-    for key in freq:
-        if freq[key] > maxf:
-            maxf = freq[key]
-            mode = key
-
-    print("Mode =", mode)
-
-
-def main():
     while True:
-        print("\n1.Input  2.Avg  3.High/Low  4.Zero  5.Mode  6.Exit")
+        print("\n LIBRARY BORROW RECORD SYSTEM")
+        print("1. Add Book Record")
+        print("2. Add Member Record")
+        print("3. avg_books by all memb")
+        print("4. Highest n lowest Borrowing")
+        print("5. Count members with 0 borrow cnt")
+        print("6. Mode\n7. Exit")
+
         ch = int(input("Enter choice: "))
 
         if ch == 1:
-            input_records()
-        elif ch == 2:
-            compute_avg()
-        elif ch == 3:
-            highest_lowest()
-        elif ch == 4:
-            zero_borrowers()
-        elif ch == 5:
-            find_mode()
-        elif ch == 6:
-            break
-        else:
-            print("Invalid choice")
+            name = input("Enter book name:")
+            books[name] = int(input("Enter times borrowed:"))
 
-main()
+        elif ch == 2:
+            name = input("Enter member name:")
+            members[name] = int(input("Enter books borrowed:"))
+
+        elif ch == 3:
+            avg_(members)
+
+        elif ch == 4:
+            High_Low(books)
+
+        elif ch == 5:
+            Zero_cnt(members)
+
+        elif ch == 6:
+            Most_freq(books)
+
+        elif ch == 7:
+            break
+
+
+Menu()
